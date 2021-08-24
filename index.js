de48b6f2662a026794e477f009f3488e1caf4560
ethanial1 <camiloeiz00@gmail.com>
2021-08-24 10:40:49 -0500

    Eventos de mongoose: open, error

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const mongoose = require('mongoose');
 
 // Conectamos a mongoDB
 // protocolo://dirección:puerto/baseDeDatos
-mongoose.connect('mongodb://localhost:27017/Store', {
+const uri = 'mongodb://localhost:27017/Store';
+
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+// Eventos de Mongoose
+// - Conectado
+mongoose.connection.on('open', _ => {
+    console.log("Database is connected to",uri);
+});
+
+// - Error en la conexión
+mongoose.connection.on('error', err => {
+    console.log(err);
+});
