f9c8200c8088fe206c20bb66366fd871742a8a9a
ethanial1 <camiloeiz00@gmail.com>
2021-08-24 10:45:06 -0500

    reestructura

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ const mongoose = require('mongoose');
 // protocolo://dirección:puerto/baseDeDatos
 const uri = 'mongodb://localhost:27017/Store';
 
+const db = mongoose.connection;
+
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+
+}).catch(err => console.log(err));
 
 // Eventos de Mongoose
-// - Conectado
-mongoose.connection.on('open', _ => {
+// - Conectado (Solo se ejecuta una vez)
+db.once('open', _ => {
     console.log("Database is connected to",uri);
 });
 
 // - Error en la conexión
-mongoose.connection.on('error', err => {
+db.on('error', err => {
     console.log(err);
 });
